31da2b539ec1778d5b020af977aae16ad352803c
dm-vb <d.buryak@gmail.com>
2023-05-03 15:53:17 +0300

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |blog|
+  Blog.create(
+    title: "My blog N #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus sed eros a scelerisque. Nullam hendrerit, diam vel sagittis rutrum, magna sapien volutpat ipsum, pellentesque mollis mauris risus nec mi. Mauris suscipit ligula quis nulla hendrerit, eu lacinia ante pulvinar. Donec laoreet tellus et semper sollicitudin. Phasellus tempor nibh sed neque gravida, faucibus venenatis quam dapibus. Suspendisse a risus luctus, semper massa quis, luctus orci. Donec dui ex, venenatis cursus risus eget, vehicula sagittis mi.
+
+Ut pellentesque mi nec dapibus mattis. Vivamus sed leo sed erat cursus congue. Ut id elit sodales mauris tempus tempor sit amet et purus. Donec vitae augue ipsum. In efficitur mi nec eros dapibus pharetra. Quisque vel vulputate ligula. Morbi fermentum nisi ligula, eget pellentesque enim scelerisque sit amet. Nullam eget eros quis urna fringilla imperdiet. Sed interdum ex id ligula finibus convallis. Quisque quis sem nec elit finibus dignissim. Duis vehicula, ipsum ac mattis ultrices, nisi mauris hendrerit augue, et lobortis tortor justo vel lorem. Sed consequat tempus pharetra. Morbi aliquet congue elit, posuere mattis mauris tincidunt non."
+  )
+end
+
+5.times do |skill|
+  Skill.create(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+9.times do |portfolio|
+  Portfolio.create(
+  title: "Portfolio title N #{portfolio}",
+  subtitle: "My grate service",
+  body:"Vivamus sed leo sed erat cursus congue. Ut id elit sodales mauris tempus tempor sit amet et purus. Donec vitae augue ipsum. In efficitur mi nec eros dapibus pharetra. Quisque vel vulputate ligula. Morbi fermentum nisi ligula, eget pellentesque enim scelerisque sit amet. Nullam eget eros quis urna fringilla imperdiet. Sed interdum ex id ligula finibus convallis. Quisque quis sem nec elit finibus dignissim. Duis vehicula, ipsum ac mattis ultrices, nisi mauris hendrerit augue, et lobortis tortor justo vel lorem. Sed consequat tempus pharetra. Morbi aliquet congue elit, posuere mattis mauris tincidunt non.",
+    main_image: "https://placehold.co/600x400",
+    thumb_image: "https://placehold.co/350x200"
+  )
+end
