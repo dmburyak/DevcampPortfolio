812ac01be706690333dd85e33a82654669c92b09
dm-vb <d.buryak@gmail.com>
2023-05-07 15:49:18 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  # get 'pages/home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
+
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
